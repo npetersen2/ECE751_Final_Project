497fa0068da7199ce8580bac3800c7c46b5b9492
Nathan Petersen <npetersen2@wisc.edu>
2021-12-07 09:47:36 -0600

    Add vivado project

---
+++ vivado/project_zedboard_1/project_zedboard_1.runs/synth_1/.Xil/base_zynq_wrapper_propImpl.xdc
@@ -0,0 +1,17 @@
+set_property SRC_FILE_INFO {cfile:C:/Users/Nathan/Documents/GitHub/AMDC-Firmware3/project_zedboard_1/project_zedboard_1.srcs/constrs_1/new/constraints.xdc rfile:../../../project_zedboard_1.srcs/constrs_1/new/constraints.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:2 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN T22 [get_ports LED[0]];
+set_property src_info {type:XDC file:1 line:3 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN T21 [get_ports LED[1]];
+set_property src_info {type:XDC file:1 line:4 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN U22 [get_ports LED[2]];
+set_property src_info {type:XDC file:1 line:5 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN U21 [get_ports LED[3]];
+set_property src_info {type:XDC file:1 line:6 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN V22 [get_ports LED[4]];
+set_property src_info {type:XDC file:1 line:7 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN W22 [get_ports LED[5]];
+set_property src_info {type:XDC file:1 line:8 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN U19 [get_ports LED[6]];
+set_property src_info {type:XDC file:1 line:9 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN U14 [get_ports LED[7]];
---
+++ vivado/project_zedboard_1/project_zedboard_1.srcs/sources_1/bd/base_zynq/ip/base_zynq_blk_mem_gen_0_0/base_zynq_blk_mem_gen_0_0_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Tue Nov 30 10:20:17 2021
+// Host        : jeff running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/Nathan/Documents/GitHub/AMDC-Firmware3/project_zedboard_1/project_zedboard_1.srcs/sources_1/bd/base_zynq/ip/base_zynq_blk_mem_gen_0_0/base_zynq_blk_mem_gen_0_0_stub.v
+// Design      : base_zynq_blk_mem_gen_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_3,Vivado 2019.1" *)
+module base_zynq_blk_mem_gen_0_0(clka, rsta, ena, wea, addra, dina, douta, rsta_busy)
+/* synthesis syn_black_box black_box_pad_pin="clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],rsta_busy" */;
+  input clka;
+  input rsta;
+  input ena;
+  input [3:0]wea;
+  input [31:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+  output rsta_busy;
+endmodule
